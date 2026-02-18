ce57ba78fcb5774fc86650271ade19e5edec8e87
ishayturk <130680079+ishayturk@users.noreply.github.com>
2026-02-18 10:27:30 +0200

    Update exam_logic.py

--- exam_logic.py
+++ exam_logic.py
@@ -1,4 +1,4 @@
-# Version: C-05 | ID: C-01
+# Version: C-06.1 | ID: C-01
 import pandas as pd
 import random
 
@@ -8,6 +8,5 @@ def get_unique_exam(df, finished_exams):
     return random.choice(available) if available else None
 
 def prepare_question_data(df, exam_col, start_idx, end_idx):
-    actual_end = min(end_idx, 25)
-    batch = df.iloc[start_idx:actual_end][['שאלה', exam_col, 'תשובה_נכונה']]
+    batch = df.iloc[start_idx:min(end_idx, 25)][['שאלה', exam_col, 'תשובה_נכונה']]
     return batch.to_dict('records')
